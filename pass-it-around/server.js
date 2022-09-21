67b7ce6ff07708c85cb51adfed5917b6db39efa9
Nate Crawford <nathanwcrawford22@gmail.com>
2022-09-21 18:13:01 -0400

    Bonus: Fun w/Bugs

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,18 +1,69 @@
+const e = require('express');
 const express = require('express');
 const port = 3000;
 const app = express();
 
-app.get("/:numberOfBottles", (req, res) => {
- let numberOfBottles = req.params.numberOfBottles;
- let nextPage = numberOfBottles - 1;
+app.set('views', './views');
+app.set('view engine', 'bugs');
 
- if (numberOfBottles > 1) {
-    res.send(`${numberOfBottles} Bottles of beer on the wall! <a href="/${nextPage}">Take one down, pass it around..</a>`)
- } else {
-    res.send(`1 bottle of beer on the wall '<a href="/99">Start Over</a>'`)
- }
+const fs = require('fs');
 
-});
+app.engine('bugs', (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+      if (err) return callback(err)
+      
+      const rendered = content.toString()
+        .replace('#title#', '<title>' + options.title + '</title>')
+        .replace('#message#', '<h1>' + options.message + '</h1>').replace('#content#','<p>'+ options.content + '</>' ).replace('#content1#','<p>'+ options.content1 + '</>' )
+      return callback(null, rendered)
+    })
+  })
+  
+
+// app.get("/:numberOfBottles", (req, res) => {
+//  let numberOfBottles = req.params.numberOfBottles;
+//  let nextPage = numberOfBottles - 1;
+
+//  if (numberOfBottles > 1) {
+//     res.send(`${numberOfBottles} Bottles of beer on the wall! <a href="/${nextPage}">Take one down, pass it around..</a>`)
+//  } else {
+//     res.send(`1 bottle of beer on the wall '<a href="/99">Start Over</a>'`)
+//  }
+
+// });
+
+app.get("/:numberOfBugs", (req, res) => {
+    let numberOfBugs = req.params.numberOfBugs;
+    let nextPage = numberOfBugs - 1;
+    let randomPage = Math.floor(Math.random() * numberOfBugs * 2);
+    let nextRandomPage = randomPage - 1;
+   
+    if (numberOfBugs == 99) {
+        res.render('template', {title: numberOfBugs + ' Bugs!', message: numberOfBugs + ' Little bugs in the code!', content: numberOfBugs + ' Little bugs..', content1: randomPage + ' bugs in the code! ' + `<a href="/${nextPage}">Fix more bugs?</a>`})
+
+    //    res.send(`${numberOfBugs} Little bugs in the code!'
+    //              ${numberOfBugs} Little bugs!
+    //              Take one down..
+    //              Patch it around..
+    //              ${randomPage} bugs in the code! <a href="/${nextPage}">Fix more bugs?</a>`)
+    } else if (numberOfBugs == 1){
+
+        res.render('template', {title: numberOfBugs + ' Bug!', message: numberOfBugs + ' Little bug in the code!', content: numberOfBugs + ' Little bug..', content1: randomPage + ' bugs in the code! ' + `<a href="/${nextRandomPage}">Fix more bugs?</a>`})
+
+        // res.send(`${numberOfBugs} Little bugs in the code! 
+        //           ${numberOfBugs} Little bugs!
+        //           Take one down..
+        //           Patch it around..
+        //           ${randomPage} bugs in the code! <a href="/${nextRandomPage}">Fix more bugs?</a>`)
+    } else if (numberOfBugs >= 0 || numberOfBugs != 1 || numberOfBugs < 99 || numberOfBugs > 99 || numberOfBugs != 99) {
+        nextRandomPage = randomPage;
+        res.render('template', {title: numberOfBugs + ' Bugs!', message: numberOfBugs + ' Little bugs in the code!', content: numberOfBugs + ' Little bugs..', content1: randomPage + ' bugs in the code! ' + `<a href="/${nextRandomPage}">Fix more bugs?</a>`})
+    } else {
+        res.send(`We need to find more bugs to FIX ASAP!! '<a href="/99">Start Over</a>'`)
+
+    }
+   
+   });
 
 app.listen(port, () => {
     console.log('Listening to Port ' + port);
